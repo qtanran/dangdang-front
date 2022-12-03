fbe9d12487b4f3baf91453d51b541b7eda91c5d4
qtanran <1240429779@qq.com>
2022-12-03 21:04:28 +0800

    ci: 修改 vite 配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, CommonServerOptions } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const fs =  require('fs')
+const dotenv = require('dotenv')
+import { DotenvParseOutput } from 'dotenv'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(mode => {
+  const envFileName:string = '.env'
+  const curEnvFileName = `${envFileName}.${mode.mode}`
+  let server: CommonServerOptions = {}
+  const envData = fs.readFileSync(curEnvFileName)
+  const envMap: DotenvParseOutput = dotenv.parse(envData)
+  console.log(envMap);
+  if (mode.mode === 'development') {
+    server = {
+      proxy: {
+        [envMap.VITE_BASE_URL]: {
+          target: 'http://192.168.11.1'
+        }
+      }
+    }
+  } else if (mode.mode === 'production') {
+    console.log('生产者环境');
+  }
+  return {
+    plugins: [vue()],
+    server
+  }
 })
